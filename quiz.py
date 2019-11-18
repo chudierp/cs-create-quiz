1ba5a11350d7da4af76b216b27cd94c27ae53e59
Chudier <chudierp@gmail.com>
2019-11-17 19:00:03 -0800

    first version complete

--- quiz.py
+++ quiz.py
@@ -24,12 +24,65 @@ def question_two():
 
     elif response != correct_answer:
         print("That was incorrect, the correct answer was " + correct_answer)
-        return 0  
+        return 0
+
+def question_three():
+    """The user enters in a numeric response to an open ended question.
+    I recommend prompting the user to enter an integer. If you choose to use floats,
+    be specific to the user about how they should enter their response
+    (i.e. rounded and/or a certain number of decimal places)."""
+
+    response = input("3. How many states are there in the United States? Please enter an integer.")
+    correct_answer = "50"
+
+    if response == correct_answer:
+        print("Correct")
+        return 1
+    elif response != correct_answer:
+        print("That was incorrect, the correct answer was " + correct_answer)
+        return 0
+
+def question_four():
+    """The user enters 1 or 0 in response to a true or false statement. Compare the response to the correct answer
+     and return a 1 if they got it right and a 0 if not."""
+
+    response = input("4. Washington, DC is the capital of the United States of America, true or false? Please enter 1 for True or 0 for False. ")
+    correct_answer = "1"
+
+    if response == correct_answer:
+        print("Correct")
+        return 1
+    elif response != correct_answer:
+        print("That was incorrect, the correct answer was " + correct_answer)
+        return 0    
+
+def question_five():
+    """The user enters in a numeric response to an open ended question.
+    I recommend prompting the user to enter an integer. If you choose to use floats,
+    be specific to the user about how they should enter their response
+    (i.e. rounded and/or a certain number of decimal places)."""
+
+    response = input("5. Trump is the __ th President of the United States. Please enter an integer.")
+    correct_answer = "45"
+
+    if response == correct_answer:
+        print("Correct")
+        return 1
+    elif response == correct_answer: 
+        print("That was incorrect, the correct answer was " + correct_answer)
+        return 0        
+
+
+
+
+
+
             
 
 #Code to run the quiz
 total_score = 0
-result = question_one() + question_two()
+result = question_one() + question_two() + question_three() + question_four()
 total_score += result
+# print("Here's how many questions you got correct out of 5 \n 4 and above means you passed the test \n anything below means you need more practice")
 print(total_score)        
 
